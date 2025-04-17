e22438eb3a667b8986627b66dedb67ca667aee25
Prakharper <158788969+Prakharper@users.noreply.github.com>
2025-04-16 20:56:37 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -42,7 +42,7 @@ global.errorm2 = '✰ 𝐀 𝐎𝐜𝐮𝐫𝐫𝐢𝐝𝐨 𝐔𝐧 𝐄𝐫�
 global.resp = '𝐀𝐪𝐮𝐢 𝐓𝐢𝐞𝐧𝐞 𝐒𝐮 𝐏𝐞𝐝𝐢𝐝𝐨 〜(^∇^〜)'
 global.espera = '✰ 𝐄𝐬𝐩𝐞𝐫𝐚 𝐔𝐧 𝐌𝐨𝐦𝐞𝐧𝐭𝐨, 𝐄𝐬𝐭𝐚𝐦𝐨 𝐄𝐧𝐯𝐢𝐚𝐧𝐝𝐨 𝐒𝐮 𝐏𝐞𝐝𝐢𝐝𝐨 (✿◠‿◠)'
 global.nombrebot = 'DeimosBot'
-global.textbot = `「 🌕 𝐃𝐞𝐢𝐦𝐨𝐬𝐁𝐨𝐭 - 𝐌𝐃 🪷🌕 」`
+global.textbot = `「 🌕 𝐃𝐞𝐢𝐦𝐨𝐬𝐁𝐨𝐭 - 𝐌𝐃 🌕 」`
 global.vs = '2.1.0'
 global.publi = '☠︎︎𝐒𝐢𝐠𝐮𝐞 𝐄𝐥 𝐂𝐚𝐧𝐚𝐥👇'
 
